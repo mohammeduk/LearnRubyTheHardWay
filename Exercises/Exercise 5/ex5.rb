b60756f570181b4e1243d41235e941600d614c01
Mohammad Usman Khan <mohammeduk@spartaglobal.co>
2017-07-18 15:50:57 +0100

    Completed Exercise 5

---
+++ Exercises/Exercise 5/ex5.rb
@@ -0,0 +1,20 @@
+name = 'Mohammad Usman Khan aka Oz'
+age = 28 # true as of 2017 - I feel 82
+height = 71 # inches
+weight = 182 #lbs
+eyes = 'Brown'
+teeth = 'White'
+hair = 'Brown'
+
+
+puts "Let's talk about #{name}."
+puts "He's #{height} inches tall"
+puts "He's #{weight} pounds heavy"
+puts "Actually that's not too heavy"
+puts "He's got #{eyes} eyes and #{hair} hair"
+puts "His teeth are usually #{teeth} depending on the coffee"
+
+puts "If I add #{age}, #{height}, and #{weight}  I get #{age + height + weight}."
+
+puts "My height in centimetes is #{height * 2.54}"
+puts "My weight in kilograms is #{weight * 0.453592}"
