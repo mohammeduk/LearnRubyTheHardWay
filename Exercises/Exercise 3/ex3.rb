38abe78b9dc03214b1114f16d07a20a2cdc20925
Mohammad Usman Khan <mohammeduk@spartaglobal.co>
2017-07-18 13:39:08 +0100

    Exercise 2 & 3 complete

---
+++ Exercises/Exercise 3/ex3.rb
@@ -0,0 +1,35 @@
+# This line prints out some text to the console log
+puts "I will now count my chickens"
+
+# The next two lines do a small equation and print out the output with some text
+puts "Hens #{25 + 30 / 6}"
+puts "Roosters #{100 - 25 * 3 % 4}"
+
+# This line prints out some text
+puts "Now I will count the eggs:"
+
+# This line prints out the sum of an equation
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# This line prints out some text
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+# This line prints out a boolean (True or False) of an equation
+puts 3 + 2 < 5 - 7
+
+# The next 2 lines prints out some text and the sum of an equation
+puts "What is 3 + 2? #{3 +2}"
+puts "What is 5 - 7? #{5 - 7}"
+
+# This line prints out some text
+puts "Oh, that's why it's false."
+
+# This line prints out some text
+puts "How about some more."
+
+# This line prints out a boolean from an equation for each question
+puts "Is it greater? #{5  > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
+
+puts 3.3 + 2.5
