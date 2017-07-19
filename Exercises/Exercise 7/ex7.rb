c795c0ba716d78287b943b955b53c8ed8e0df5b2
Mohammad Usman Khan <mohammeduk@spartaglobal.co>
2017-07-19 10:47:44 +0100

    Completed Exercise 7

---
+++ Exercises/Exercise 7/ex7.rb
@@ -0,0 +1,23 @@
+# Outputs strings to the console
+puts "Oz was in love with chocolate bars"
+puts "His waist was far and #{'wide'}"
+puts "He went everywhere chocolate did"
+puts "." * 5
+
+# Stores values in variables
+end1 = "M"
+end2 = "i"
+end3 = "l"
+end4 = "k"
+end5 = "y"
+end6 = " "
+end7 = "W"
+end8 = "a"
+end9 = "y"
+end10 = " "
+end11 = "😋"
+
+# Outputs value on one line with no carriage return
+print end1 + end2 + end3 + end4 + end5 + end6
+# Outputs value on one line with carriage return
+puts end7 + end8 + end9 + end10 + end11
