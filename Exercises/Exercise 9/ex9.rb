348cf6d6e971ae8986fb196581674043a997c9b7
Mohammad Usman Khan <mohammeduk@spartaglobal.co>
2017-07-19 14:10:04 +0100

    Completed Exercise 9

---
+++ Exercises/Exercise 9/ex9.rb
@@ -0,0 +1,12 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\Jul\nAug"
+
+puts "Here are the days: #{days}"
+puts "Here are the months #{months}"
+
+puts %q{
+  Foo bar
+  This is line 2
+  And 3
+  ...And 4!
+}
